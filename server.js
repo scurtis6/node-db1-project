78e74b6a05a113651cd2a1d1c448caf644fc4b56
Sierra Curtis <sierracurtis91@gmail.com>
2020-02-17 16:11:20 -0500

    finished queries.sql

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const db = require("./data/dbConfig.js");
+
+const AccountRouter = require('./accounts/account-router');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', AccountRouter);
+
+server.get('/', (req, res) => {
+    res.send('<h2>node-db1-project is up and running<h2>')
+});
 
 module.exports = server;
