36f61793f38416d79038f752ae3f44492db4decf
Sierra Curtis <sierracurtis91@gmail.com>
2020-02-17 18:26:16 -0500

    put request is working

--- accounts/account-router.js
+++ accounts/account-router.js
@@ -15,9 +15,49 @@ router.get('/', (req, res) => {
         res.status(500).json({ error: 'failed to get the list of accounts' });
     });
 });
-// router.get('/', (req, res) => {})
-// router.post('/', (req, res) => {})
-// router.put('/', (req, res) => {})
+
+router.get('/:id', (req, res) => {
+    // select * from accounts where id = :id
+    db.select('*')
+    .from('accounts')
+    .where({ id: req.params.id })
+    .first() // grab the first item on the returned array
+    .then(acc => {
+        res.status(200).json(acc)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: 'failed to get the account by ID' });
+    });
+});
+
+router.post('/', (req, res) => {
+    // add a account
+    const accInfo = req.body
+    db('accounts').insert(accInfo, 'id') // will generate a warning on console whrn using sqlite, ignore that
+    .then(acc => {
+        res.status(201).json(acc)
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'failed to add account' })
+    })
+});
+
+router.put('/:id', (req, res) => {
+    // update account
+    const id = req.params.id
+    const changes = req.body
+    db('accounts')
+    .where({ id })
+    .update(changes)
+    .then(acc => {
+        res.status(200).json(acc)
+    })
+    .catch(err => {
+        res.status(500).json({ error: 'failed to update the account' })
+    })
+});
+
 // router.delete('/', (req, res) => {})
 
 module.exports = router;
